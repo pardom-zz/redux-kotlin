1aa88327134611c0bb566092e3d039ee66a362e6
Guillaume Lung <guillaume@soundcloud.com>
2016-12-10 13:32:35 +0100

    make ktlint happy

--- lib/src/test/kotlin/redux/api/VanillaStoreSpecsTest.kt
+++ lib/src/test/kotlin/redux/api/VanillaStoreSpecsTest.kt
@@ -3,7 +3,7 @@ package redux.api
 class VanillaStoreSpecsTest : redux.api.StoreTest() {
 
     override fun <S : Any> createStore(reducer: Reducer<S>, state: S): Store<S> {
-       return redux.createStore(reducer, state, Store.Enhancer { it })
+        return redux.createStore(reducer, state, Store.Enhancer { it })
     }
 
 }
